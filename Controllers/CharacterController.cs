5c3f75ac53ba963322cf3fd06d917f773aad126c
Stu Last <stuart.last@pendennis.com>
2022-11-08 14:30:56 +0000

    Initial commit

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet.rpg.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CharacterController : ControllerBase
+	{
+		private static Character knight = new Character();
+
+		[HttpGet(Name = "GetCharacter")]
+		public ActionResult<Character> Get()
+		{
+			return Ok(knight);
+		}
+	}
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace dotnet.rpg.Models
+{
+
+	public enum RpgClass
+	{
+		Knight = 1,
+		Mage = 2,
+		Cleric = 3
+	}
+}
